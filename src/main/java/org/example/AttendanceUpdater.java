39643e363e951d3da35595b4fb343d004ee965d3
MOHAMMED-ASIK <mohammedasikakkim@gmail.com>
2024-02-13 22:40:32 +0530

    success 🚀

--- src/main/java/org/example/AttendanceUpdater.java
+++ src/main/java/org/example/AttendanceUpdater.java
@@ -2,16 +2,19 @@ package org.example;
 
 import java.sql.*;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 public class AttendanceUpdater {
     public static void updateAttendance(int empId, LocalTime Time) {
             boolean isLoginTime = false;
+            LocalTime logintime = null;
             String selectSql = "SELECT * FROM Attendance WHERE emp_id = ? AND login_time IS NOT NULL";
             try (Connection connection = DBConnector.getConnection();
                  PreparedStatement selectStatement = connection.prepareStatement(selectSql)) {
                 selectStatement.setInt(1, empId);
                 ResultSet resultSet = selectStatement.executeQuery();
                 if (resultSet.next()) {
+                     logintime= resultSet.getTime(2).toLocalTime();
                     isLoginTime = true; // Employee has already logged in
                 }
             } catch (SQLException e) {
@@ -23,6 +26,10 @@ public class AttendanceUpdater {
             // Insert the time into the appropriate column based on whether it's a login or logout time
             String insertSql;
             if (isLoginTime) {
+               if( logintime.until(Time,ChronoUnit.MINUTES)<5){
+                   System.out.println("Try again later");
+                   System.exit(0);
+               }
                 insertSql = "UPDATE Attendance SET logout_time = ? WHERE emp_id = ? AND logout_time IS NULL";
             } else {
                 insertSql = "INSERT INTO Attendance (emp_id, login_time) VALUES (?, ?)";
